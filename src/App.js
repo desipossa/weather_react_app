d62913af2e4c2713e618aabe8b81912dc7d1fa8e
desipossa <c1jang@hanmail.net>
2023-09-15 11:53:14 +0900

    20230915

--- src/App.js
+++ src/App.js
@@ -21,17 +21,27 @@ const App = () => {
             yyyy: today.getFullYear(),
             mm: today.getMonth() + 1,
             dd: today.getDate(),
-            hour: today.getHours() - 1
+            hour: today.getHours() + 1
         }
 
-        const base = `${ymd.yyyy}${ymd.mm > 9 ? ymd.mm : '0' + ymd.mm}${ymd.dd}`;
+        const base = `${ymd.yyyy}${ymd.mm < 10 ? '0' + ymd.mm : ymd.mm}${ymd.dd}`;
         const base_time = `${ymd.hour < 10 ? '0' : ''}${ymd.hour}`;
 
-        const r = await axios.get(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=json&base_date=${base}&base_time=${base_time}00&nx=${nx}&ny=${ny}`);
+        //현재 날씨.
+        //const r = await axios.get(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=json&base_date=${base}&base_time=${base_time}00&nx=${nx}&ny=${ny}`);
+
+        //단기예보
+        const r = await axios.get(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${key}&pageNo=1&numOfRows=1000&dataType=json&base_date=${base}&base_time=${base_time}00&nx=${nx}&ny=${ny}`);
+        //const r = await axios.get(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=nmPIjJ%2Bj0FufPiP6k4BLPlq3n%2B46QZN%2B6hgSINrrxqk3nNwnoHX2ynqX6Dlgr3xFeivGPus2vgmh6Ifx1vdu1g%3D%3D&pageNo=1&numOfRows=1000&dataType=XML&base_date=20230915&base_time=0500&nx=55&ny=127`);
         const d = await r.data.response.body.items.item;
-        //console.log(r, d);
+        console.log(r);
 
         setData(d);
+
+
+        //한시간씩 주는 온도 데이터를 뽑아서 배열로 만듬.
+
+        const DD = d.map(it => it.category == 'TMP');
 
     }
 
@@ -48,15 +58,15 @@ const App = () => {
             }
             강수형태 :
             {
-                data[0]?.obsrValue
+                data[4]?.fcstValue
             }
             기온:
             {
-                data[3]?.obsrValue
+                data[3]?.fcstValue
             }
             습도:
             {
-                data[1]?.obsrValue
+                data[1]?.fcstValue
             }
 
         </div>
